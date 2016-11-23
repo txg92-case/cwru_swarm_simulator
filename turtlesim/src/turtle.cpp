457c2651c48b39960d4edf707f49d954f2a75c42
jj <remix226@gmail.com>
2016-11-22 21:32:35 -0500

    added noise to motion model and sensory model. Swarm sim control needs to be updated to match that of the filter

--- turtlesim/src/turtle.cpp
+++ turtlesim/src/turtle.cpp
@@ -5,14 +5,14 @@
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the Willow Garage, Inc. nor the names of its
- *       contributors may be used to endorse or promote products derived from
- *       this software without specific prior written permission.
+ *		 * Redistributions of source code must retain the above copyright
+ *			 notice, this list of conditions and the following disclaimer.
+ *		 * Redistributions in binary form must reproduce the above copyright
+ *			 notice, this list of conditions and the following disclaimer in the
+ *			 documentation and/or other materials provided with the distribution.
+ *		 * Neither the name of the Willow Garage, Inc. nor the names of its
+ *			 contributors may be used to endorse or promote products derived from
+ *			 this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -36,6 +36,17 @@
 #define DEFAULT_PEN_G 0xb8
 #define DEFAULT_PEN_B 0xff
 
+#define NOISY 			true
+
+#define POSITION_ERROR 		1
+#define ROT_ERROR 			.1
+
+#define RD_POSITION_ERROR 	1
+#define RD_ROT_ERROR 		.1
+
+#define RD_POS_VEL_ERROR 	2
+#define RD_ROT_VEL_ERROR 	.4
+
 namespace swarm_sim
 {
 
@@ -50,174 +61,205 @@ Turtle::Turtle(const ros::NodeHandle& nh, const QImage& turtle_image, const QPoi
 , evil_(false)
 , pen_(QColor(DEFAULT_PEN_R, DEFAULT_PEN_G, DEFAULT_PEN_B))
 {
-  pen_.setWidth(3);
+	pen_.setWidth(3);
 
-  velocity_sub_ = nh_.subscribe("cmd_vel", 1, &Turtle::velocityCallback, this);
-  pose_pub_ = nh_.advertise<Pose>("pose", 1);
-  color_pub_ = nh_.advertise<Color>("color_sensor", 1);
-  set_pen_srv_ = nh_.advertiseService("set_pen", &Turtle::setPenCallback, this);
-  teleport_relative_srv_ = nh_.advertiseService("teleport_relative", &Turtle::teleportRelativeCallback, this);
-  teleport_absolute_srv_ = nh_.advertiseService("teleport_absolute", &Turtle::teleportAbsoluteCallback, this);
+	velocity_sub_ = nh_.subscribe("cmd_vel", 1, &Turtle::velocityCallback, this);
+	pose_pub_ = nh_.advertise<Pose>("pose", 1);
+	pose_pub_noisy_ = nh_.advertise<Pose>("pose_noisy", 1);
+	color_pub_ = nh_.advertise<Color>("color_sensor", 1);
+	set_pen_srv_ = nh_.advertiseService("set_pen", &Turtle::setPenCallback, this);
+	teleport_relative_srv_ = nh_.advertiseService("teleport_relative", &Turtle::teleportRelativeCallback, this);
+	teleport_absolute_srv_ = nh_.advertiseService("teleport_absolute", &Turtle::teleportAbsoluteCallback, this);
 
-  meter_ = turtle_image_.height();
-  rotateImage();
+	meter_ 	= turtle_image_.height();
+
+	nd_l 	= std::normal_distribution<float> (0, POSITION_ERROR); //mean followed by stdiv
+	nd_r 	= std::normal_distribution<float> (0, ROT_ERROR);
+
+	nd_rd_l = std::normal_distribution<float> (0, RD_POSITION_ERROR); //mean followed by stdiv
+	nd_rd_r = std::normal_distribution<float> (0, RD_ROT_ERROR);
+
+	nd_rv_l = std::normal_distribution<float> (0, RD_POS_VEL_ERROR); //mean followed by stdiv
+	nd_rv_r = std::normal_distribution<float> (0, RD_ROT_VEL_ERROR);
+
+	rotateImage();
 }
 
 
 void Turtle::velocityCallback(const geometry_msgs::Twist::ConstPtr& vel)
 {
-  last_command_time_ = ros::WallTime::now();
-  lin_vel_ = vel->linear.x;
-  ang_vel_ = vel->angular.z;
+	last_command_time_ = ros::WallTime::now();
+	lin_vel_ = vel->linear.x;
+	ang_vel_ = vel->angular.z;
 }
 
 bool Turtle::setPenCallback(swarm_sim::SetPen::Request& req, swarm_sim::SetPen::Response&)
 {
-  pen_on_ = !req.off;
-  if (req.off)
-  {
-    return true;
-  }
+	pen_on_ = !req.off;
+	if (req.off)
+	{
+		return true;
+	}
 
-  QPen pen(QColor(req.r, req.g, req.b));
-  if (req.width != 0)
-  {
-    pen.setWidth(req.width);
-  }
+	QPen pen(QColor(req.r, req.g, req.b));
+	if (req.width != 0)
+	{
+		pen.setWidth(req.width);
+	}
 
-  pen_ = pen;
-  return true;
+	pen_ = pen;
+	return true;
 }
 
 bool Turtle::teleportRelativeCallback(swarm_sim::TeleportRelative::Request& req, swarm_sim::TeleportRelative::Response&)
 {
-  teleport_requests_.push_back(TeleportRequest(0, 0, req.angular, req.linear, true));
-  return true;
+	teleport_requests_.push_back(TeleportRequest(0, 0, req.angular, req.linear, true));
+	return true;
 }
 
 bool Turtle::teleportAbsoluteCallback(swarm_sim::TeleportAbsolute::Request& req, swarm_sim::TeleportAbsolute::Response&)
 {
-  teleport_requests_.push_back(TeleportRequest(req.x, req.y, req.theta, 0, false));
-  return true;
+	teleport_requests_.push_back(TeleportRequest(req.x, req.y, req.theta, 0, false));
+	return true;
 }
 
 void Turtle::rotateImage()
 {
-  QTransform transform;
-  transform.rotate(-orient_ * 180.0 / PI + 90.0);
-  turtle_rotated_image_ = turtle_image_.transformed(transform);
+	QTransform transform;
+	transform.rotate(-orient_ * 180.0 / PI + 90.0);
+	turtle_rotated_image_ = turtle_image_.transformed(transform);
 }
 
 bool Turtle::update(double dt, QPainter& path_painter, const QImage& path_image, qreal canvas_width, qreal canvas_height)
 {
-  bool modified = false;
-  qreal old_orient = orient_;
+	bool modified = false;
+	qreal old_orient = orient_;
 
-  // first process any teleportation requests, in order
-  V_TeleportRequest::iterator it = teleport_requests_.begin();
-  V_TeleportRequest::iterator end = teleport_requests_.end();
-  for (; it != end; ++it)
-  {
-    const TeleportRequest& req = *it;
+	// first process any teleportation requests, in order
+	V_TeleportRequest::iterator it = teleport_requests_.begin();
+	V_TeleportRequest::iterator end = teleport_requests_.end();
+	for (; it != end; ++it)
+	{
+		const TeleportRequest& req = *it;
 
-    QPointF old_pos = pos_;
-    if (req.relative)
-    {
-      orient_ += req.theta;
-      pos_.rx() += std::sin(orient_ + PI/2.0) * req.linear;
-      pos_.ry() += std::cos(orient_ + PI/2.0) * req.linear;
-    }
-    else
-    {
-      pos_.setX(req.pos.x());
-      pos_.setY(std::max(0.0, static_cast<double>(canvas_height - req.pos.y())));
-      orient_ = req.theta;
-    }
+		QPointF old_pos = pos_;
+		if (req.relative)
+		{
+			orient_ += req.theta;
 
-    if (pen_on_)
-    {
-      path_painter.setPen(pen_);
-      path_painter.drawLine(pos_ * meter_, old_pos * meter_);
-    }
-    modified = true;
-  }
+			pos_.rx() += std::sin(orient_ + PI/2.0) * req.linear;
+			pos_.ry() += std::cos(orient_ + PI/2.0) * req.linear;
+		}
+		else
+		{
+			pos_.setX(req.pos.x());
+			pos_.setY(std::max(0.0, static_cast<double>(canvas_height - req.pos.y())));
+			orient_ = req.theta;
+		}
 
-  teleport_requests_.clear();
+		if (pen_on_)
+		{
+			path_painter.setPen(pen_);
+			path_painter.drawLine(pos_ * meter_, old_pos * meter_);
+		}
+		modified = true;
+	}
 
-  if (ros::WallTime::now() - last_command_time_ > ros::WallDuration(1.0))
-  {
-    lin_vel_ = 0.0;
-    ang_vel_ = 0.0;
-  }
+	teleport_requests_.clear();
 
-  QPointF old_pos = pos_;
+	if (ros::WallTime::now() - last_command_time_ > ros::WallDuration(1.0))
+	{
+		lin_vel_ = 0.0;
+		ang_vel_ = 0.0;
+	}
 
-  orient_ = std::fmod(orient_ + ang_vel_ * dt, 2*PI);
-  pos_.rx() += std::sin(orient_ + PI/2.0) * lin_vel_ * dt;
-  pos_.ry() += std::cos(orient_ + PI/2.0) * lin_vel_ * dt;
+	QPointF old_pos = pos_;
 
-  // Clamp to screen size
-  if (pos_.x() < 0 || pos_.x() > canvas_width ||
-      pos_.y() < 0 || pos_.y() > canvas_height)
-  {
-    ROS_WARN("Oh no! I hit the wall! (Clamping from [x=%f, y=%f])", pos_.x(), pos_.y());
-  }
+	if (NOISY) {
+		orient_ = std::fmod(orient_ + (ang_vel_ + nd_r(generator)) * dt, 2*PI);
+		pos_.rx() += std::sin(orient_ + PI/2.0) * (lin_vel_+ nd_l(generator)) * dt;
+		pos_.ry() += std::cos(orient_ + PI/2.0) * (lin_vel_+ nd_l(generator)) * dt;
+	}
+	else {
+		orient_ = std::fmod(orient_ + ang_vel_ * dt, 2*PI);
+		pos_.rx() += std::sin(orient_ + PI/2.0) * lin_vel_ * dt;
+		pos_.ry() += std::cos(orient_ + PI/2.0) * lin_vel_ * dt;
+	}
 
-  pos_.setX(std::min(std::max(static_cast<double>(pos_.x()), 0.0), static_cast<double>(canvas_width)));
-  pos_.setY(std::min(std::max(static_cast<double>(pos_.y()), 0.0), static_cast<double>(canvas_height)));
 
-  // Publish pose of the turtle
-  Pose p;
-  p.x = pos_.x();
-  p.y = canvas_height - pos_.y();
-  p.theta = orient_;
-  p.linear_velocity = lin_vel_;
-  p.angular_velocity = ang_vel_;
-  p.wolf = evil_;
-  pose_pub_.publish(p);
+	// Clamp to screen size
+	if (pos_.x() < 0 || pos_.x() > canvas_width ||
+			pos_.y() < 0 || pos_.y() > canvas_height)
+	{
+		ROS_WARN("Oh no! I hit the wall! (Clamping from [x=%f, y=%f])", pos_.x(), pos_.y());
+	}
 
-  // Figure out (and publish) the color underneath the turtle
-  {
-    Color color;
-    QRgb pixel = path_image.pixel((pos_ * meter_).toPoint());
-    color.r = qRed(pixel);
-    color.g = qGreen(pixel);
-    color.b = qBlue(pixel);
-    color_pub_.publish(color);
-  }
+	pos_.setX(std::min(std::max(static_cast<double>(pos_.x()), 0.0), static_cast<double>(canvas_width)));
+	pos_.setY(std::min(std::max(static_cast<double>(pos_.y()), 0.0), static_cast<double>(canvas_height)));
 
-  ROS_DEBUG("[%s]: pos_x: %f pos_y: %f theta: %f", nh_.getNamespace().c_str(), pos_.x(), pos_.y(), orient_);
+	// Publish pose of the turtle
+	Pose p;
+	p.x = pos_.x();
+	p.y = canvas_height - pos_.y();
+	p.theta = orient_;
+	p.linear_velocity = lin_vel_;
+	p.angular_velocity = ang_vel_;
+	p.wolf = evil_;
 
-  if (orient_ != old_orient)
-  {
-    rotateImage();
-    modified = true;
-  }
-  if (pos_ != old_pos)
-  {
-    if (pen_on_)
-    {
-      path_painter.setPen(pen_);
-      path_painter.drawLine(pos_ * meter_, old_pos * meter_);
-    }
-    modified = true;
-  }
+	pose_pub_.publish(p);
 
-  return modified;
+	Pose p_noisy;
+	p_noisy.x = pos_.x() + nd_rd_l(generator);
+	p_noisy.y = canvas_height - pos_.y() + nd_rd_l(generator);
+	p_noisy.theta = orient_ + nd_rd_r(generator);
+	p_noisy.linear_velocity = lin_vel_ + nd_rv_l(generator);
+	p_noisy.angular_velocity = ang_vel_ + nd_rv_r(generator);
+	p_noisy.wolf = evil_;
+
+	pose_pub_noisy_.publish(p_noisy);
+
+	// Figure out (and publish) the color underneath the turtle
+	{
+		Color color;
+		QRgb pixel = path_image.pixel((pos_ * meter_).toPoint());
+		color.r = qRed(pixel);
+		color.g = qGreen(pixel);
+		color.b = qBlue(pixel);
+		color_pub_.publish(color);
+	}
+
+	ROS_DEBUG("[%s]: pos_x: %f pos_y: %f theta: %f", nh_.getNamespace().c_str(), pos_.x(), pos_.y(), orient_);
+
+	if (orient_ != old_orient)
+	{
+		rotateImage();
+		modified = true;
+	}
+	if (pos_ != old_pos)
+	{
+		if (pen_on_)
+		{
+			path_painter.setPen(pen_);
+			path_painter.drawLine(pos_ * meter_, old_pos * meter_);
+		}
+		modified = true;
+	}
+
+	return modified;
 }
 
 void Turtle::paint(QPainter& painter)
 {
-  QPointF p = pos_ * meter_;
-  p.rx() -= 0.5 * turtle_rotated_image_.width();
-  p.ry() -= 0.5 * turtle_rotated_image_.height();
-  painter.drawImage(p, turtle_rotated_image_);
+	QPointF p = pos_ * meter_;
+	p.rx() -= 0.5 * turtle_rotated_image_.width();
+	p.ry() -= 0.5 * turtle_rotated_image_.height();
+	painter.drawImage(p, turtle_rotated_image_);
 }
 
 void Turtle::setEvil(bool evil, const QImage& turtle_image)
 {
-  this->evil_ = evil;
-  this->turtle_image_ = turtle_image;
+	this->evil_ = evil;
+	this->turtle_image_ = turtle_image;
 }
 
 }
